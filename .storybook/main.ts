cbcbe39b346ea8688f57519a934a3efedd65454c
Joe Collicoat <jcollicoat@gmail.com>
2022-11-18 16:08:24 +1300

    Inject var in dev

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,18 +2,19 @@ import type { StorybookConfig } from "@storybook/react-vite";
 import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
-  framework: '@storybook/react-vite',
-  stories: ['../**/Button.stories.[tj]sx'],
-  addons: ['@storybook/addon-essentials'],
+  framework: "@storybook/react-vite",
+  stories: ["../**/Button.stories.[tj]sx"],
+  addons: ["@storybook/addon-essentials"],
   viteFinal(config) {
-      const merged = mergeConfig(config, {
-          resolve: {
-              alias: {
-                  path: 'path-browserify',
-              },
-          },
-      });
-      return merged;
+    const merged = mergeConfig(config, {
+      define: { "import.meta.env.OTHER_DEFINE": JSON.stringify("otherdefine") },
+      resolve: {
+        alias: {
+          path: "path-browserify",
+        },
+      },
+    });
+    return merged;
   },
 };
 
